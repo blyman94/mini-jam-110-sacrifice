14cd2de3edf61ce845ad816483b369b87f289a63
Brandon Lyman <66562222+blyman94@users.noreply.github.com>
2022-07-08 10:25:30 -0400

    Player control+movement (#1)
    * Create PlayerControlMovementScene
    
    * Add player look
    
    Player can now look around the scene with the mouse. The player character game objects rotates about its y axis as the look direction changes.
    
    * Add player movement
    
    Player can now move around 3D space using WASD, Arrow Keys, or Gamepad input. Movement will always be relative to the player's forward axis based on its current rotation. Gamepad input added to look action as well.
    
    * Add player interaction
    
    The player can now interact with GameObjects on the "Interactable" layer by using any of the below listed inputs. The Interactor script prints the name of the target GameObject as a placeholder for future functionality.
    
    + Left Click
    + Right Click
    + E
    + Spacebar
    + Gamepad Button South (A on an Xbox controller)
    + Gamepad Right Shoulder
    + Gamepad Right Trigger

--- MJ110Project/Assets/Scripts/Control/Player.cs
+++ MJ110Project/Assets/Scripts/Control/Player.cs
@@ -4,15 +4,6 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public Mover3D Mover3D;
+    public Interactor Interactor;
 }
--- MJ110Project/Assets/Scripts/Control/PlayerController.cs
+++ MJ110Project/Assets/Scripts/Control/PlayerController.cs
@@ -1,18 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private Player player;
+
+    #region MonoBehaviour Methods
+    private void Start()
     {
-        
+        LockCursor();
+    }
+    #endregion
+
+    public void LockCursor()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OnInteractionInput(InputAction.CallbackContext context)
     {
-        
+        if (context.started)
+        {
+            player.Interactor.Interact();
+        }
+    }
+
+    public void OnMovementInput(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            player.Mover3D.MoveInput = context.ReadValue<Vector2>();
+        }
+        else
+        {
+            player.Mover3D.MoveInput = Vector2.zero;
+        }
     }
 }
