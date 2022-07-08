82129e43ee603f02cc16fa8c73ce3a0724132d48
Brandon Lyman <lyman.br@northeastern.edu>
2022-07-08 09:53:13 -0400

    Add player movement
    Player can now move around 3D space using WASD, Arrow Keys, or Gamepad input. Movement will always be relative to the player's forward axis based on its current rotation. Gamepad input added to look action as well.

--- MJ110Project/Assets/Scripts/Movement/Mover3D.cs
+++ MJ110Project/Assets/Scripts/Movement/Mover3D.cs
@@ -4,15 +4,65 @@ using UnityEngine;
 
 public class Mover3D : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [Header("Component References")]
+    [SerializeField] private CharacterController characterController;
+
+    [Header("Movement Params")]
+    [SerializeField] private float maxSpeed = 5;
+    [Tooltip("For instant movement, set acceleration = -1")]
+    [SerializeField] private float acceleration = -1;
+
+    private float newSpeed;
+
+    public Vector2 MoveInput { get; set; }
+
+    #region MonoBehaviour Methods
+    private void Update()
     {
-        
+        CalculateNewMoveSpeed();
+        ApplyMotion();
+    }
+    #endregion
+
+    private void ApplyMotion()
+    {
+        Vector3 planarMovement =
+            ((transform.forward * MoveInput.y) +
+            (transform.right * MoveInput.x)).normalized *
+            (newSpeed * Time.deltaTime);
+        characterController.Move(planarMovement);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void CalculateNewMoveSpeed()
     {
-        
+        float targetSpeed = maxSpeed;
+
+        if (MoveInput == Vector2.zero)
+        {
+            targetSpeed = 0.0f;
+        }
+
+        if (acceleration != -1 && acceleration > 0)
+        {
+            float currentPlanarSpeed =
+                new Vector3(characterController.velocity.x, 0.0f,
+                characterController.velocity.z).magnitude;
+
+            if (currentPlanarSpeed < targetSpeed - 0.1f ||
+            currentPlanarSpeed > targetSpeed + 0.1f)
+            {
+                newSpeed =
+                    Mathf.Lerp(currentPlanarSpeed, targetSpeed,
+                    acceleration * Time.deltaTime);
+            }
+            else
+            {
+                newSpeed = targetSpeed;
+            }
+        }
+        else
+        {
+            newSpeed = targetSpeed;
+        }
     }
 }
