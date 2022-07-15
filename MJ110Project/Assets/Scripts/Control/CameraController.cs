b1974fc29857a81dc682c5a7f2acdb5d7e512cfb
Brandon Lyman <66562222+blyman94@users.noreply.github.com>
2022-07-15 09:14:25 -0400

    Character controller fix (#8)
    * Fixed character controller + audio
    
    * MVPB

---
+++ MJ110Project/Assets/Scripts/Control/CameraController.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField] private Transform target;
+    [SerializeField] private float clampYMin = -35;
+    [SerializeField] private float clampYMax = 35;
+    [SerializeField] private MenuModelRotator menuModelRotator;
+    [SerializeField] private InventoryToggle inventoryToggle;
+
+    [SerializeField] private bool invertYAxis = false;
+    [SerializeField] private float lookSensitivityX = 1;
+    [SerializeField] private float lookSensitivityY = 1;
+
+    public bool Disabled { get; set; } = false;
+
+    private float mouseX;
+    private float mouseY;
+    private float prevMouseX;
+
+    private void LateUpdate()
+    {
+        CameraControl();
+    }
+
+    private void CameraControl()
+    {
+        if (!menuModelRotator.IsInMenu && 
+            !inventoryToggle.InventoryOpen && !Disabled)
+        {
+            mouseX += Input.GetAxis("Mouse X") * lookSensitivityY;
+            mouseY += Input.GetAxis("Mouse Y") * lookSensitivityX * (invertYAxis ? 1 : -1);
+            mouseY = Mathf.Clamp(mouseY, clampYMin, clampYMax);
+
+            target.rotation = Quaternion.Euler(mouseY, mouseX, 0);
+            prevMouseX = mouseX;
+        }
+    }
+}
